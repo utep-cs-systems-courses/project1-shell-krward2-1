3a73ebd824115e54bca9d5423ce74e77e445a189
krward2 <43589922+krward2@users.noreply.github.com>
2020-09-13 13:07:15 -0700

    Basic redirection add. Will refactor this spaghetti.

--- shell/shell.py
+++ shell/shell.py
@@ -18,6 +18,27 @@ while True:
         continue
     if command == 'exit':
         sys.exit()
+    if '>' in command:
+        command, output = command.split('>')
+        command = command.split()
+        output = output.strip()
+        pid = os.fork()
+
+        if pid == 0:
+            os.close(1)
+            os.open(output, os.O_CREAT | os.O_WRONLY)
+            os.set_inheritable(1, True)
+            if os.path.exists(command[0]):
+                os.execve(command[0], command, os.environ.copy())
+            else:
+                for directory in path:
+                    executable = directory + '/' + command[0]
+                    if os.path.exists(executable):
+                        os.execve(executable, command, os.environ.copy())
+        else:
+            os.wait()
+
+        continue
     command = command.split()
     if command[0] == 'cd':
         os.chdir(command[1])

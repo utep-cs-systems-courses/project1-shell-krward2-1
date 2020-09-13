e704d1855877f8618bb2710cacd7a2ac069b012d
krward2 <43589922+krward2@users.noreply.github.com>
2020-09-13 16:25:40 -0700

    Clean-up. Pipe still broken.

---
+++ shell/shellRefactoring.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import re
+
+
+def execute(command):
+    # Check if command[0] is itself a path
+    if os.path.exists(command[0]):
+            os.execve(command[0], command, os.environ.copy())
+    else:
+        for directory in path:
+            executable = directory + '/' + command[0]
+            if os.path.exists(executable):
+                    os.execve(executable, command, os.environ.copy())
+
+
+def redirect(command):
+    command, output = command.split('>')
+    command = command.split()
+    output = output.strip()
+    os.close(1)
+    os.open(output, os.O_CREAT | os.O_WRONLY)
+    os.set_inheritable(1, True)
+
+    execute(command)
+
+
+def split(command, option=None):
+    pid = os.fork()
+    if pid == 0:
+        if option is None:
+            execute(command)
+        if option == 'redirect':
+            redirect(command)
+    else:
+        os.wait()
+
+
+def initialize():
+    try:
+        sys.ps1
+    except AttributeError:
+        sys.ps1 = '$ '
+    global path
+    path = os.environ['PATH']
+    path = path.split(':')
+
+
+def shell():
+    initialize()
+    while True:
+        command = input(sys.ps1)
+        if command == '':
+            continue
+        if command == 'exit':
+            sys.exit()
+        if '>' in command:
+            split(command, option='redirect')
+        split(command)
+
+shell()
